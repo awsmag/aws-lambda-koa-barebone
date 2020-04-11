346f18c3d76903854d8d8dc8b8caf72cf98632b6
Simranjeet Singh <simranjeet2589@gmail.com>
2020-04-11 11:31:36 +0530

    [Adding koa-mw-logger]

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const Koa = require("koa");
 const cors = require("@koa/cors");
 const bodyparser = require("koa-bodyparser");
 const mount = require("koa-mount");
+const loggerMw = require("koa-mw-logger");
 
 const middlewares = require("./middlewares");
 const services = require("./services");
@@ -10,6 +11,7 @@ const app = new Koa();
 
 app.use(cors());
 app.use(bodyparser());
+app.use(loggerMw());
 
 /**
  * Mount all the middlewares on the umbrella app
